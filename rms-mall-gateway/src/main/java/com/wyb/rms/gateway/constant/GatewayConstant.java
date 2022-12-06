bb301c30abb3325aa4e8dd3a97a84f11f9673543
wangyb <1501987783@qq.com>
2022-12-06 09:18:21 +0800

    网关路径调整

--- rms-mall-gateway/src/main/java/com/wyb/rms/gateway/constant/GatewayConstant.java
+++ rms-mall-gateway/src/main/java/com/wyb/rms/gateway/constant/GatewayConstant.java
@@ -8,6 +8,7 @@ public class GatewayConstant {
     /** 登录 uri */
     public static final String LOGIN_URI = "/rms-mall-gateway/login";
 
+
     /** 注册 uri */
     public static final String REGISTER_URI = "/rms-mall-gateway/register";
 
